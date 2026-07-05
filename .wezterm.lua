03921f0f2f87761919abfba39999efe1de6993d4
wjwjjwwj <jie.wang167@gmail.com>
2026-07-05 12:16:52 -0700

    update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,6 +10,10 @@ local config = wezterm.config_builder()
 config.initial_cols = 120
 config.initial_rows = 28
 
+config.window_background_opacity = 0.8
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+
 -- or, changing the font size and color scheme.
 config.font = wezterm.font("Hack Nerd Font", { weight = "Regular" })
 config.max_fps = 120
